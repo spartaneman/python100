49c61e97aeb776fdf9c9c0e92f81f28c98b9a803
emanRuiz <Emanuel.Ruiz@student.csulb.edu>
2023-07-19 19:49:39 -0700

    Challenge 5 complete with shuffle method

---
+++ UdemyPython100/Day5/day5Challenge.py
@@ -0,0 +1,32 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#combine all 
+all_char = [letters, numbers, symbols]
+total_c = nr_letters + nr_symbols + nr_numbers
+
+password = []
+for i in range(nr_letters):
+    password.append(letters[random.randint(0,25)])
+
+for j in range(nr_numbers):
+    password.append(numbers[random.randint(0, 9)])
+
+for k in range(nr_symbols):
+    password.append(symbols[random.randint(0, 8)])
+
+random.shuffle(password)
+output = ""
+for letter in password:
+    output += letter
+
+print(output)
+
--- UdemyPython100/Day5/loops.py
+++ UdemyPython100/Day5/loops.py
@@ -13,3 +13,35 @@ for height in heights:
 
 loop_mean = total/i
 print(loop_mean)    
+
+
+# get highest value
+# max()
+scores = [45, 56, 78, 83, 90, 56, 87,78,45, 78, 99, 87, 46, 35]
+
+max , min = scores[0], scores[0]
+
+for score in scores:
+    if score > max:
+        max = score
+    if score < min:
+        min = score
+print(max, min)
+
+# For loops with range exclusive end
+# 
+for num in range(1, 49, 3):
+    print(num)
+
+
+# print every even value between 1 and 100
+even = 0 
+for num in range(1, 101):
+    if(num %2 == 0):
+        even += num
+print(even)
+
+neven = 0 
+for num in range(2, 101, 2):
+    neven += num
+print(neven)
